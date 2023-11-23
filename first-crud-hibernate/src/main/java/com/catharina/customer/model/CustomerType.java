8e97205bcc6f20215515ec01611d0429f0feaaf3
Catharina <catharina_@hotmail.com.br>
2023-11-23 20:22:44 -0300

    add new entity

---
+++ first-crud-hibernate/src/main/java/com/catharina/customer/model/CustomerType.java
@@ -0,0 +1,54 @@
+package com.catharina.customer.model;
+
+import java.util.List;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "customer_type_tb")
+public class CustomerType {
+	
+	@Id
+	@Column(name = "customer_type_id")
+	private Integer id;
+	
+	@Column(name = "customer_type_name")
+	private String name;
+	
+	@OneToMany(mappedBy = "customerType")
+	private List<Customer> customers;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Customer> getCustomers() {
+		return customers;
+	}
+
+	public void setCustomers(List<Customer> customers) {
+		this.customers = customers;
+	}
+
+	@Override
+	public String toString() {
+		return "CustomerType [id=" + id + ", name=" + name + ", customers=" + customers + "]";
+	}
+
+
+
+	
+
+}
